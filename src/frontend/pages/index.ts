154ec17a604b1f441db0eecf3b607b400b42d074
isarpsiyahofficial <isarpsiyahofficial@gmail.com>
2026-08-03 09:09:36 +0300

    chore: export frontend pages

---
+++ src/frontend/pages/index.ts
@@ -0,0 +1,4 @@
+export { DashboardPage, ContactsPage, FilesPage, NotificationsPage, ReportsPage } from './core';
+export { WhatsAppPage } from './whatsapp';
+export { KnowledgePage, AiPage } from './knowledgeAi';
+export { SettingsPage } from './settings';
