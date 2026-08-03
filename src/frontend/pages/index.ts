af523fb0d5edd4a3bc774771379240530ac58bc4
isarpsiyahofficial <isarpsiyahofficial@gmail.com>
2026-08-03 13:28:21 +0300

    refactor: route dashboards to transparent Neuron views

--- src/frontend/pages/index.ts
+++ src/frontend/pages/index.ts
@@ -1,4 +1,5 @@
-export { DashboardPage, ContactsPage, FilesPage, NotificationsPage, ReportsPage } from './core';
+export { ContactsPage, FilesPage, NotificationsPage } from './core';
+export { DashboardPage, ReportsPage } from './dashboardReports';
 export { WhatsAppPage } from './whatsapp';
 export { KnowledgePage } from './knowledgeAi';
 export { AiPage } from './aiPage';
