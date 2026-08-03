19f78e31e7d6f5992f916241b30049052bd93818
isarpsiyahofficial <isarpsiyahofficial@gmail.com>
2026-08-03 09:42:06 +0300

    test: augment Workerd environment with migration binding

--- tests/worker/env.d.ts
+++ tests/worker/env.d.ts
@@ -1,7 +1,9 @@
-import type { Env } from '../../src/worker/types';
-
-declare module 'cloudflare:workers' {
-  interface ProvidedEnv extends Env {
-    TEST_MIGRATIONS: D1Migration[];
+declare global {
+  namespace Cloudflare {
+    interface Env {
+      TEST_MIGRATIONS: D1Migration[];
+    }
   }
 }
+
+export {};
